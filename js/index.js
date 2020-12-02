427d0def050d8c118018f2562d0a31922287cf35
Claudiuts <sarb.claudiu391@gmail.com>
2020-12-02 15:53:49 -0600

    done

--- js/index.js
+++ js/index.js
@@ -1,12 +1,47 @@
-let h1 = document.querySelector('h1');
-h1.addEventListener('mouseover', function(event) {
-  event.target.style.color = 'gold';
+const navLink = document.querySelector("nav");
+
+navLink.addEventListener("click", (evt) => {
+  evt.target.style.color = "red";
 });
 
-let body = document.querySelector('body');
-body.addEventListener('keydown', function(event) {
-  if (event.keyCode == 66) {
-    h1.innerHTML = 'The Crazy Train';
-  }
+const header = document.querySelector(".intro");
+
+header.addEventListener("click", (evt) => {
+  evt.target.style.color = "royalblue";
+  evt.target.style.backgroundColor = "yellow";
 });
 
+const textContent = document.querySelector(".text-content");
+
+textContent.addEventListener("mouseover", (evt) => {
+  evt.target.style.backgroundColor = "black";
+  evt.target.style.color = "white";
+  evt.target.font = "font-smaller";
+});
+
+const textContent2 = document.querySelector(".inverse-content");
+
+textContent2.addEventListener("mouseenter", (evt) => {
+  evt.target.style.backgroundColor = "black";
+  evt.target.style.color = "white";
+  evt.target.font = "font-smaller";
+});
+
+const h1 = document.querySelector("h1");
+
+h1.addEventListener("dblclick", (evt) => {
+  evt.target.textContent = "Bus Fun";
+});
+
+const doom = document.querySelector(".content-destination h2");
+
+doom.addEventListener("mouseover", (evt) => {
+  evt.target.textContent = " Your dooms day vaction starts now";
+  evt.target.style.color = "darkred";
+});
+
+const btn = document.querySelector(".content-pick");
+btn.querySelector(".btn");
+btn.addEventListener("mouseenter", (evt) => {
+  evt.target.textContent = "turn back now";
+});
